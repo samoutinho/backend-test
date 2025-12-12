46a8c60b80d7b13d2de4ed78c5464f516c5d3c61
Samuel Alves Moutinho <samuca.moutinho@gmail.com>
2025-12-12 10:02:47 -0300

    feat: adicionar middleware de logging e validações

---
+++ src/infrastructure/middleware/logger.middleware.ts
@@ -0,0 +1,27 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl, ip } = req;
+    const startTime = Date.now();
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const duration = Date.now() - startTime;
+      const message = `${method} ${originalUrl} ${statusCode} ${duration}ms - ${ip}`;
+
+      if (statusCode >= 400) {
+        this.logger.error(message);
+      } else {
+        this.logger.log(message);
+      }
+    });
+
+    next();
+  }
+}
+
